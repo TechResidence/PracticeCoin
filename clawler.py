bb53eb2a32247a9dc96bd983474a08e8eab46e6a
Kotaro Kamiya <pon.de.kotapon.6@gmail.com>
2016-06-26 19:44:53 +0900

    get price within infinite loop

--- clawler.py
+++ clawler.py
@@ -1,6 +1,7 @@
 import urllib3
 from bs4 import BeautifulSoup
-
+import time
+from datetime import datetime
 TARGET_URL = 'https://zaif.jp/instant_exchange_btc_jpy/1'
 
 def get_soup(url, http):
@@ -8,10 +9,16 @@ def get_soup(url, http):
   soup = BeautifulSoup(html, 'html.parser')
   return soup
 
-http = urllib3.PoolManager()
-soup = get_soup(TARGET_URL, http)
-bid = soup.find(id='btc_bid').get_text()
-ask = soup.find(id='btc_ask').get_text()
+# ask: buy, bid: sell
+def get_bitcoin_price(url=TARGET_URL):
+  http = urllib3.PoolManager()
+  while True:
+    soup = get_soup(url, http)
+    bid = soup.find(id='btc_bid').get_text()
+    ask = soup.find(id='btc_ask').get_text()
+    now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    time.sleep(2)
+    print(now, 'bid: ', bid, 'ask: ', ask)
 
-print("bid: ", bid, "ask: ", ask)
 
+get_bitcoin_price()

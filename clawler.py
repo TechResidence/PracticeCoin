150947ad3ff7cdec58a04f17522062c274de18eb
czmizogaki <mizogaki-masahito@cyber-z.co.jp>
2016-06-26 19:37:03 +0900

    modify

--- clawler.py
+++ clawler.py
@@ -10,8 +10,15 @@ def get_soup(url, http):
 
 http = urllib3.PoolManager()
 soup = get_soup(TARGET_URL, http)
+<<<<<<< Updated upstream
 bid = soup.find(id='btc_bid').get_text()
 ask = soup.find(id='btc_ask').get_text()
 
 print("bid: ", bid, "ask: ", ask)
 
+=======
+soup.find(id='bfPriceAsk_0')
+result = soup.find(id='btc_bid')
+
+print(result)
+>>>>>>> Stashed changes

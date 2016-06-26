1004b855467530fd3dccb5f9bd8b77607f3e09da
Kotaro Kamiya <pon.de.kotapon.6@gmail.com>
2016-06-26 20:45:01 +0900

    refactoring

--- clawler.py
+++ clawler.py
@@ -1,8 +1,6 @@
-import urllib3
-import certifi
 from bs4 import BeautifulSoup
-import time
 from datetime import datetime
+
 TARGET_URL = 'https://zaif.jp/instant_exchange_btc_jpy/1'
 
 def get_soup(url, http):
@@ -10,22 +8,10 @@ def get_soup(url, http):
   soup = BeautifulSoup(html, 'html.parser')
   return soup
 
-
-def get_bitcoin_price(url=TARGET_URL):
-  """ ask: buy, bid: sell """
-  http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-  while True:
+def get_bitcoin_price(http, url=TARGET_URL):
+    """ ask: buy, bid: sell """
     soup = get_soup(url, http)
     bid = soup.find(id='btc_bid').get_text()
     ask = soup.find(id='btc_ask').get_text()
     now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    time.sleep(2)
-    print(now, 'bid: ', bid, 'ask: ', ask)
-
-
-def main():
-  get_bitcoin_price()
-
-
-if __name__ == "__main__":
-   main()
+    return {'timestamp': now, 'bid': bid, 'ask': ask}
